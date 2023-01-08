094f804e60f6b537f76550aaee9ad6a4d7fbf56c
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2023-01-08 16:10:48 +0400

    first Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        for (int num = 2; num <= n; num++) {
+            if (isPrime(num)) {
+                System.out.println(num + " არის მარტივი");
+            } else {
+                System.out.println(num + " არის შედგენილი");
+            }
+        }
+        }
+        public static boolean isPrime(int number){
+            for (int j = 2; j <= Math.sqrt(number); j++) {
+                if (number % j == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
